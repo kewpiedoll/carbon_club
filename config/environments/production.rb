c8d464f4290ec401134b61c4a12797cc590e4667
Chris Ashfield <biodieselchris@gmail.com>
2014-05-29 00:10:48 -0700

    added evironments/production.rb host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 CarbonClub::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'stormy-brushlands-4790.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
